497bff85bcf1560495cd357b1b8a53e9eabe3046
Stefan Witschel <Stefan.Witschel@softwareag.com>
2022-04-19 12:01:13 +0200

    Enhanced Description, proper linking

--- md-generator/markdown_generator.py
+++ md-generator/markdown_generator.py
@@ -19,41 +19,47 @@ from mdutils.mdutils import MdUtils
 from mdutils import Html
 from datetime import datetime, timezone
 
+
 class MarkdownGenerator():
 
     def __init__(self):
         self.cat_list = []
+
     def read_md_file(self):
         file = self.mdFile.read_md_file('./README.md')
         return file
 
     def create_md_file(self, repos):
 
-        #repos = self.filter_repo_list(repos)
+        # repos = self.filter_repo_list(repos)
 
         mdFile = MdUtils(file_name='../README.md', title='Cumulocity IoT Open-Source Repository Overview')
-        #mdFile.new_header(level=1, title='Cumulocity IoT Open-Source Repository Overview')
+        # mdFile.new_header(level=1, title='Cumulocity IoT Open-Source Repository Overview')
         mdFile.new_paragraph(
-            "This Repository generates on a daily basis a Table of all Open-Source Repositories for Cumulocity-IoT having the topic 'cumulocity-iot'. It should give a brief overview of all available IoT Open-Source Repositories for Cumulocity IoT including additional content at TechCommunity."
-            "Please use this [link](https://github.com/SoftwareAG/cumulocity-os-repo-overview/blob/main/README.md) to have a proper view on the table.")
+            "This Repository generates on a daily basis a Table of all Open-Source Repositories for Cumulocity-IoT "
+            "having the topic 'cumulocity-iot'. It should give a brief overview of all available IoT Open-Source "
+            "Repositories for Cumulocity IoT including additional content at TechCommunity.")
+
+        mdFile.new_paragraph("Please use this [link](https://github.com/SoftwareAG/cumulocity-os-repo-overview/blob/main/README.md) to have a proper view on the table.")
         mdFile.new_paragraph(
-            "Number of Open-Source Repos: **"+str(len(repos))+"**")
+            "Number of Open-Source Repos: **" + str(len(repos)) + "**")
         mdFile.new_header(level=1, title='Open-Source Repository Table')
         list = self.build_table_list(repos)
-        mdFile.new_table(8, len(repos)+1, list)
+        mdFile.new_table(9, len(repos) + 1, list)
         mdFile.create_md_file()
 
     def filter_repo_list(self, repos):
         filtered_list = []
         for repo in repos:
             if repo['visibility'] == 'public':
-                if "cumulocity" in repo['name'] or "c8y" in repo['name'] or "cumulocity-iot" in repo['topics'] or "cumulocity" in repo['topics']:
+                if "cumulocity" in repo['name'] or "c8y" in repo['name'] or "cumulocity-iot" in repo[
+                    'topics'] or "cumulocity" in repo['topics']:
                     filtered_list.append(repo)
         filtered_list = sorted(filtered_list, key=lambda d: d['stargazers_count'], reverse=True)
         return filtered_list
 
     def build_table_list(self, repos):
-        text_list = ['Repo Name', 'Description', 'Category', 'Language', 'Last Updated', 'Stars',
+        text_list = ['Repo Name', 'Description', 'Category', 'Topics', 'Language', 'Last Updated', 'Stars',
                      'References', 'Relation']
         for repo in repos:
             name = repo['name']
@@ -93,7 +99,7 @@ class MarkdownGenerator():
                 cat_list.append(cat)
             self.cat_list = cat_list
             if len(cat_list) > 0:
-                cat = " ,".join(str(cat) for cat in cat_list)
+                cat = " ".join(str(cat) for cat in cat_list)
             else:
                 cat = 'Other'
             if len(topics) > 0:
@@ -110,10 +116,10 @@ class MarkdownGenerator():
             owner = repo['owner']['login']
             if owner == 'SoftwareAG':
                 relation = 'SAG-Org Repo'
-            elif owner == 'reubenmiller' or  owner == 'TryManuZ':
+            elif owner == 'reubenmiller' or owner == 'TryManuZ':
                 relation = 'Trusted-Contributor Repo'
             else:
                 relation = 'Open-Source Repo'
-            #relation = 'SAG-Org Repo'
-            text_list.extend(["["+name+"]("+url+")", desc, cat, lang, date_string, stars, reference, relation])
+            # relation = 'SAG-Org Repo'
+            text_list.extend(["[" + name + "](" + url + ")", desc, cat, topic_string, lang, date_string, stars, reference, relation])
         return text_list
